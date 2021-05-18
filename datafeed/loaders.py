47948e033a8fc920ef61a731dd7a68d91b9d97bb
Jacob Bieker <jacob@bieker.tech>
2021-05-18 05:09:10 -0700

    Add comments

--- datafeed/loaders.py
+++ datafeed/loaders.py
@@ -2,4 +2,7 @@
 
 Methods for loadings the various open 3D AV datasets into a common format for use throughout this repo
 
+This is setup for the Audi a2d2, Waymo Open Dataset, NuScenes, Argoverse, ApolloScape, Bosch's Boxy Dataset, and Pandaset
+
 """
+
