3d8f10fe5c6790f3276bba6f18c6e45316ce4279
Jacob Bieker <jacob@bieker.tech>
2021-05-19 04:47:00 -0700

    Add environment file

--- datafeed/loaders.py
+++ datafeed/loaders.py
@@ -4,5 +4,8 @@ Methods for loadings the various open 3D AV datasets into a common format for us
 
 This is setup for the Audi a2d2, Waymo Open Dataset, NuScenes, Argoverse, ApolloScape, Bosch's Boxy Dataset, and Pandaset
 
+Attempts to load the different modules for loading in the datasets, but should just pass if the setup doesn't exist
+
 """
+
 
