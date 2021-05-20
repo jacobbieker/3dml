2585713a82f44f455a179a27c9eccc96644ccd32
Jacob Bieker <jacob@bieker.tech>
2021-05-20 07:07:51 -0700

    Extract some metadata

--- frames/cameras.py
+++ frames/cameras.py
@@ -36,18 +36,24 @@ class A2D2Camera(Camera):
         pass
 
     def load_metadata(self, config: Dict[str, Any]):
-        pass
+        # get parameters from config file
+        self.matrix = \
+            np.asarray(config['cameras'][self.name]['CamMatrix'])
+        self.orig_matrix = \
+            np.asarray(config['cameras'][self.name]['CamMatrixOriginal'])
+        self.distortion = \
+            np.asarray(config['cameras'][self.name]['Distortion'])
+        self.lens = config['cameras'][self.name]['Lens']
 
     def project(self, points: np.ndarray):
-        if self.name in ['front_left', 'front_center', \
-                         'front_right', 'side_left', \
+        if self.name in ['front_left', 'front_center',
+                         'front_right', 'side_left',
                          'side_right', 'rear_center']:
             if self.lens == 'fisheye':
-                return cv2.fisheye.undistortImage(points, self.orig_matrix, \
+                return cv2.fisheye.undistortImage(points, self.orig_matrix,
                                                   D=self.distortion, Knew=self.matrix)
             elif self.lens == 'telecam':
-                return cv2.undistort(points, self.orig_matrix, \
-                                     distCoeffs=self.distortion, newCameraMatrix=self.matrix)
+                return cv2.undistort(points, self.orig_matrix, distCoeffs=self.distortion, newCameraMatrix=self.matrix)
             else:
                 return points
         else:
